65fc3fdd948d08602665a896c51e72d88cdd5e70
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2023-05-07 18:41:15 -0500

    added statement to define blockstate instead of using mixin

--- src/main/java/online/maestoso/microtonalnoteblocks/MicrotonalNoteblocks.java
+++ src/main/java/online/maestoso/microtonalnoteblocks/MicrotonalNoteblocks.java
@@ -1,18 +1,26 @@
 package online.maestoso.microtonalnoteblocks;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+import virtuoel.statement.api.StateRefresher;
 
 public class MicrotonalNoteblocks implements ModInitializer {
+	public static final IntProperty SCALE = IntProperty.of("scale", 1, 24);
+	public static final IntProperty OCTAVE = IntProperty.of("octave", 0, 1);
 	public static Item TUNING_FORK = new Item(new QuiltItemSettings().maxCount(1));
 	public void onInitialize(ModContainer mod) {
+		StateRefresher.INSTANCE.addBlockProperty(Blocks.NOTE_BLOCK, SCALE, 12);
+		StateRefresher.INSTANCE.addBlockProperty(Blocks.NOTE_BLOCK, OCTAVE, 0);
+		StateRefresher.INSTANCE.reorderBlockStates();
 		Registry.register(Registries.ITEM, new Identifier("microtonalnoteblocks:tuning_fork"), TUNING_FORK);
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS_AND_UTILITIES).register(content -> content.addItem(TUNING_FORK));
 	}
--- src/main/java/online/maestoso/microtonalnoteblocks/mixin/NoteBlockMixin.java
+++ src/main/java/online/maestoso/microtonalnoteblocks/mixin/NoteBlockMixin.java
@@ -4,7 +4,6 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.NoteBlock;
-import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
@@ -14,46 +13,47 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import online.maestoso.microtonalnoteblocks.MicrotonalNoteblocks;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import static net.minecraft.block.NoteBlock.*;
+import static online.maestoso.microtonalnoteblocks.MicrotonalNoteblocks.*;
 
 @Mixin(NoteBlock.class)
 public class NoteBlockMixin extends Block {
-	private static final IntProperty SCALE = IntProperty.of("scale", 0, 24);
-	private static final IntProperty NOTE = IntProperty.of("note", 0, 48);
-	public NoteBlockMixin(Settings settings) {
-		super(settings);
-	}
-	@ModifyArg(method = "<init>", at= @At(value = "INVOKE", target = "Lnet/minecraft/block/NoteBlock;setDefaultState(Lnet/minecraft/block/BlockState;)V"))
-	public BlockState microtonalnoteblocks$setDefaultState(BlockState state) {
-		return this.stateManager.getDefaultState()
-				.with(INSTRUMENT, NoteBlockInstrument.HARP)
-				.with(NOTE, 0)
-				.with(SCALE, 12)
-				.with(POWERED, false);
-	}
+	@Shadow
+	@Final
+	public static IntProperty NOTE;
 	@Inject(method = "appendProperties", at = @At("TAIL"))
 	public void microtonalnoteblocks$injectAddProperties(StateManager.Builder<Block, BlockState> builder, CallbackInfo ci) {
 		builder.add(SCALE);
+		builder.add(OCTAVE);
+	}
+	public NoteBlockMixin(Settings settings) {
+		super(settings);
 	}
 	@ModifyVariable(method = "onSyncedBlockEvent", at = @At("STORE"))
 	private float microtonalnoteblocks$adjustPitch(float f, @Local BlockState state, @Local(ordinal = 2) int i) {
 		int scale = state.get(SCALE);
-		return (float)Math.pow(2.0, (double)(i - scale) / (double) scale);
+		int note = i + 24 * state.get(OCTAVE);
+		return (float)Math.pow(2.0, (double)(note - scale) / (double) scale);
 	}
 	@ModifyVariable(method = "onUse", at = @At("STORE"), argsOnly = true)
 	private BlockState microtonalnoteblocks$resetTuneAtMax(BlockState state) {
+		if(state.get(SCALE) > 12 && state.get(NOTE) == 23) {
+			state = state.with(OCTAVE, 1);
+		}
 		if(state.get(NOTE) == state.get(SCALE) * 2 + 1) {
-			return state.with(NOTE, 0);
+			state = state
+					.with(NOTE, 0)
+					.with(OCTAVE, 0);
 		}
 		return state;
 	}
