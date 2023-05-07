7f2ef88fc8c262ca7175fd0b06d86c8ccf81b2aa
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2023-05-06 20:04:09 -0500

    configure build and stuff

--- src/main/kotlin/online/maestoso/microtonalnoteblocks/MicrotonalNoteblocks.kt
+++ src/main/kotlin/online/maestoso/microtonalnoteblocks/MicrotonalNoteblocks.kt
@@ -1,12 +1,12 @@
-package com.example.example_mod
+package online.maestoso.microtonalnoteblocks
 
 import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-object ExampleMod : ModInitializer {
-    val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
+object MicrotonalNoteblocks : ModInitializer {
+    val LOGGER: Logger = LoggerFactory.getLogger("MicrotonalNoteblocks")
 
     override fun onInitialize(mod: ModContainer) {
         LOGGER.info("Hello Quilt world from {}!", mod.metadata()?.name())
