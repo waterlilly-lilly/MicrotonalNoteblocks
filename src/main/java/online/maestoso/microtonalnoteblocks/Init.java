6fb40282db2e2bd862550815ada3f167d7fb4962
Lilly Rosaline <lilly.rosaline@waterlilly.dev>
2023-05-06 20:47:15 -0500

    de-kotlinify

---
+++ src/main/java/online/maestoso/microtonalnoteblocks/Init.java
@@ -0,0 +1,11 @@
+package online.maestoso.microtonalnoteblocks;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class Init implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
